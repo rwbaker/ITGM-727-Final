10859cd19b43fca087bc8c32a41d1d1476df9949
Richard Baker <rwbaker@ge.com>
2018-08-19 11:35:46 -0500

    Updated footer text

--- www/includes/footer.php
+++ www/includes/footer.php
@@ -5,28 +5,33 @@
  *
  */
 
+// echo '
+//  <div class="">
+//  <div>
+//    <strong>Public:</strong>
+//    <a href="/index.php">Home </a> •
+//    <a href="/login.php">Login</a> •
+//    <a href="/success.php">Success</a>
+//  </div>
+//  <div>
+//    <strong>Private:</strong>
+//    <a href="/private/index.php">Home (List)</a> •
+//    <a href="/private/add.php">Add</a> •
+//    <a href="/private/details.php">Details</a> •
+//    <a href="/private/edit.php">Edit</a>
+//  </div>
+//  <div>
+//    <strong>Admin</strong>
+//    <a href="/admin/index.php">Home (All Users)</a> •
+//    <a href="/admin/migraines.php">Users Migraines</a> •
+//    <a href="/admin/details.php">Details</a> •
+//    <a href="/admin/adduser.php">Add User</a>
+//  </div>
+//  </div>';
+
 ?>
-<div class="">
-<div>
-  <strong>Public:</strong>
-  <a href="/index.php">Home </a> •
-  <a href="/login.php">Login</a> •
-  <a href="/success.php">Success</a>
-</div>
-<div>
-  <strong>Private:</strong>
-  <a href="/private/index.php">Home (List)</a> •
-  <a href="/private/add.php">Add</a> •
-  <a href="/private/details.php">Details</a> •
-  <a href="/private/edit.php">Edit</a>
-</div>
-<div>
-  <strong>Admin</strong>
-  <a href="/admin/index.php">Home (All Users)</a> •
-  <a href="/admin/migraines.php">User's Migraines</a> •
-  <a href="/admin/details.php">Details</a> •
-  <a href="/admin/adduser.php">Add User</a>
 
-
-</div>
+<div class="container-fluid">
+<hr />
+  <span class="text-muted">Richard W Baker • ITGM-727 Databases and Dynamic Website Programming</span>
 </div>
